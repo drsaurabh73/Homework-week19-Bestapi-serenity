9e62d4e390df98136327968be53d978af44ff3a4
drsaurabh73 <drsaurabh73@gmail.com>
2022-06-21 20:01:06 +0100

    homework-week19-BestApi-Serenity

--- src/test/java/com/localhost/cucumber/steps/MyStoreSteps.java
+++ src/test/java/com/localhost/cucumber/steps/MyStoreSteps.java
@@ -11,7 +11,7 @@ import org.junit.Assert;
 
 import java.util.HashMap;
 
-import static org.hamcrest.Matchers.hasValue;
+import static org.hamcrest.Matchers.*;
 
 public class MyStoreSteps {
 
@@ -61,21 +61,82 @@ public class MyStoreSteps {
         HashMap<Object, Object> services = new HashMap<>();
         services.put("Marks", "8");
         services.put("Gentleman", "10");
-        name = name  + "_updated";
-        ValidatableResponse response = storeSteps.updateStore(storeID,name, type, address, address2, city, state, zip, lat, lng, hours, services);
+        name = name + "_updated";
+        ValidatableResponse response = storeSteps.updateStore(storeID, name, type, address, address2, city, state, zip, lat, lng, hours, services);
         HashMap<String, Object> studentMap = storeSteps.getStoreInfoByFirstname(storeID);
         Assert.assertThat(studentMap, hasValue(name));
     }
 
     @When("^User can Delete new product using DELETE method in stores application$")
     public void userCanDeleteNewProductUsingDELETEMethodInStoresApplication() {
-        response =  storeSteps.deleteProduct(storeID);
+        response = storeSteps.deleteProduct(storeID);
         response.assertThat().statusCode(200);
     }
 
     @And("^User verify that the product is deleted successfully from stores$")
     public void userVerifyThatTheProductIsDeletedSuccessfullyFromStores() {
-        response =  storeSteps.deleteProduct(storeID);
+        response = storeSteps.deleteProduct(storeID);
         response.assertThat().statusCode(404);
+    }
+
+    @Then("^verify that if the total is equal to (\\d+)$")
+    public void verifyThatIfTheTotalIsEqualTo(int expected) {
+        response = storeSteps.getAllstores();
+        int actual = response.extract().path("total");
+        Assert.assertEquals(expected, actual);
+    }
+
+    @And("^verify that if the stores of limit is equal to (\\d+)$")
+    public void verifyThatIfTheStoresOfLimitIsEqualTo(int expected) {
+        response = storeSteps.getAllstores();
+        int actual = response.extract().path("limit");
+        Assert.assertEquals(expected, actual);
+    }
+
+    @And("^Check the multiple ‘Names’ in the ArrayList \\(Roseville, Burnsville, Maplewood\\)$")
+    public void checkTheMultipleNamesInTheArrayListRosevilleBurnsvilleMaplewood() {
+        response.body("data.name", hasItem("Roseville"))
+                .body("data.name", hasItem("Burnsville"))
+                .body("data.name", hasItem("Maplewood"));
+
+    }
+
+    @And("^check the single name in the Array list \\(Inver Grove Heights\\)$")
+    public void checkTheSingleNameInTheArrayListInverGroveHeights() {
+        response.body("data[1].name",equalTo("Inver Grove Heights"));
+    }
+
+    @And("^Verify the storied=(\\d+) inside storeservices of the third store of second services$")
+    public void verifyTheStoriedInsideStoreservicesOfTheThirdStoreOfSecondServices(int expected) {
+        response = storeSteps.getAllstores();
+        int actual = response.extract().path("data[2].services[1].storeservices.storeId");
+        Assert.assertEquals(expected, actual);
+    }
+
+    @And("^Check hash map values ‘createdAt’ inside storeservices map where store name = Roseville$")
+    public void checkHashMapValuesCreatedAtInsideStoreservicesMapWhereStoreNameRoseville() {
+        response.body("data[2].services.storeservices.createdAt", hasItem("2016-11-17T17:57:09.417Z"));
+    }
+
+    @And("^Verify the serviceId = 4 for the 7th store of forth service$")
+    public void verifyTheServiceIdForTheThStoreOfForthService(int expected) {
+        response = storeSteps.getAllstores();
+        int actual = response.extract().path("data[6].services[3].storeservices.serviceId");
+        Assert.assertEquals(expected, actual);
+    }
+
+    @And("^Verify the state = MN of forth store$")
+    public void verifyTheStateMNOfForthStore() {
+        response.body("data[3].state",equalTo("MN"));
+    }
+
+    @And("^Verify the store name = Rochester of (\\d+)th store$")
+    public void verifyTheStoreNameRochesterOfThStore(int arg0) {
+        response.body("data[8].name",equalTo("Rochester"));
+    }
+
+    @And("^Verify the storeId = 11 for the 6th store$")
+    public void verifyTheStoreIdForTheThStore() {
+        response.body("data[5].services.storeservices.storeId", hasItems("11"));
     }
 }
--- src/test/java/resources/feature/Storeinfo.feature
+++ src/test/java/resources/feature/Storeinfo.feature
@@ -21,3 +21,18 @@ Feature: Testing different products on the Best Api Application
     When User can Delete new product using DELETE method in stores application
     Then User can get back a valid status code 200 of stores
     And User verify that the product is deleted successfully from stores
+
+    Scenario: I verify following dat response
+      Given I am on Homepage of application products
+      When User send a GET Request to list endpoint stores
+      Then verify that if the total is equal to 1566
+      And  verify that if the stores of limit is equal to 10
+      And check the single name in the Array list (Inver Grove Heights)
+      And Check the multiple ‘Names’ in the ArrayList (Roseville, Burnsville, Maplewood)
+      And Verify the storied=7 inside storeservices of the third store of second services
+      And Check hash map values ‘createdAt’ inside storeservices map where store name = Roseville
+      And Verify the state = MN of forth store
+      And Verify the store name = Rochester of 9th store
+      And Verify the storeId = 11 for the 6th store
+      And Verify the serviceId = 4 for the 7th store of forth service
+
